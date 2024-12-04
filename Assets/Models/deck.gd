e15597bccc357bb12e9ff9511a5a8fbadfd87ccd
Kubeva <kubeva171@gmail.com>
2024-12-05 00:08:25 +0100

    cos

--- Assets/Models/deck.gd
+++ Assets/Models/deck.gd
@@ -1,4 +1,4 @@
 extends Node
 class_name Deck
 
-var UserDeck : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
+@onready var userDeckResource : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
--- Assets/Scripts/deck_menu.gd
+++ Assets/Scripts/deck_menu.gd
@@ -1,9 +1,42 @@
 extends MarginContainer
 
-@onready var UserDeck = Deck.new()
+@onready var userDeckResource : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
 
-@onready var image: Sprite2D = $Sprite2D
-@onready var name_label: Label = $Label
+@onready var image: TextureRect = $VBoxContainer2/HBoxContainer/CardContainer/Image
+@onready var border: TextureRect = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border
+@onready var cardName: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/NameRect/NameLabel
+@onready var attack: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/AttackRect/AttackLabel
+@onready var cost: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/CostRect/CostLabel
+@onready var health: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/HealthRect/HealthLabel
+@onready var cardNamesList : ItemList = $VBoxContainer2/HBoxContainer/ScrollContainer/CardNamesList
+@onready var cardCointainer : HBoxContainer = $VBoxContainer2/HBoxContainer/CardContainer
+
+# 0Name, 1Rarity, 2Type, 3Cost, 4HP, 5Attack, 6Image
 
 func _ready() -> void:
-	name_label.set_text("grzechu");
+	for key in userDeckResource.CardsStats.keys():
+		var cardInfo = userDeckResource.CardsStats[key]
+		cardNamesList.add_item(cardInfo[0])
+
+func cardChosen(index: int) -> void:
+	var cardKey := cardNamesList.get_item_text(index) as String
+	cardKey = cardKey.replace(" ","")
+	var cardInfo = userDeckResource.CardsStats[cardKey]
+	image.texture = load(cardInfo[6])
+	var borderImage = set_border_image(cardInfo[2])
+	border.texture = load(borderImage)
+	image.show()
+	attack.text = str(cardInfo[5])
+	cost.text = str(cardInfo[3])
+	health.text = str(cardInfo[4])
+	cardName.text = cardInfo[0]
+	
+
+func set_border_image(type) -> String:
+	if(type == "Normal"):
+		return str("res://Assets/CardAssets/Borders/NormalBorder.png")
+	elif(type == "Fast"):
+		return str("res://Assets/CardAssets/Borders/FastBorder.png")
+	elif(type == "Bait"):
+		return  str("res://Assets/CardAssets/Borders/BaitBorder.png")
+	return "Damn"
