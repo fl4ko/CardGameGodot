656868443c5b3c58e8f6c48d6dece5194998d499
Kubeva <kubeva171@gmail.com>
2024-12-05 21:35:14 +0100

    Dodanie wybierania kart do puli i dodano asset

--- Assets/Scripts/deck_menu.gd
+++ Assets/Scripts/deck_menu.gd
@@ -8,30 +8,58 @@ extends MarginContainer
 @onready var attack: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/AttackRect/AttackLabel
 @onready var cost: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/CostRect/CostLabel
 @onready var health: Label = $VBoxContainer2/HBoxContainer/CardContainer/Image/Border/HealthRect/HealthLabel
+@onready var userCardMax: Label = $VBoxContainer2/HBoxContainer/GridContainer/HBoxContainer/UserCardMax
+@onready var userCardDeck: Label = $VBoxContainer2/HBoxContainer/GridContainer/HBoxContainer/UserCardDeck
 @onready var cardNamesList : ItemList = $VBoxContainer2/HBoxContainer/ScrollContainer/CardNamesList
 @onready var cardCointainer : HBoxContainer = $VBoxContainer2/HBoxContainer/CardContainer
+@onready var add : Button = $VBoxContainer2/HBoxContainer/GridContainer/HBoxContainer2/Add
+@onready var remove : Button = $VBoxContainer2/HBoxContainer/GridContainer/HBoxContainer2/Remove
 
-# 0Name, 1Rarity, 2Type, 3Cost, 4HP, 5Attack, 6Image
+# 0Name, 1Rarity, 2Type, 3Cost, 4HP, 5Attack, 6Image, 7HowManyCardsUserHas, 8CardsInDeck
+
+var cardInfo
+var cardKey
 
 func _ready() -> void:
 	for key in userDeckResource.CardsStats.keys():
-		var cardInfo = userDeckResource.CardsStats[key]
+		cardInfo = userDeckResource.CardsStats[key]
 		cardNamesList.add_item(cardInfo[0])
 	image.hide()
+	add.hide()
+	remove.hide()
 
 func cardChosen(index: int) -> void:
-	var cardKey := cardNamesList.get_item_text(index) as String
+	cardKey = cardNamesList.get_item_text(index) as String
 	cardKey = cardKey.replace(" ","")
-	var cardInfo = userDeckResource.CardsStats[cardKey]
+	cardKey = cardKey.replace("-","")
+	cardInfo = userDeckResource.CardsStats[cardKey]
 	image.texture = load(cardInfo[6])
 	var borderImage = set_border_image(cardInfo[2])
 	border.texture = load(borderImage)
 	image.show()
+	add.show()
+	remove.show()
 	attack.text = str(cardInfo[5])
 	cost.text = str(cardInfo[3])
 	health.text = str(cardInfo[4])
 	cardName.text = cardInfo[0]
+	userCardMax.text = "Avaliable Cards: " + str(cardInfo[7])
+	userCardDeck.text = "In Deck: " + str(cardInfo[8])
 	
+func cardAdded() -> void:
+	if(userDeckResource.CardsStats[cardKey][8] >= 3 || 
+	userDeckResource.CardsStats[cardKey][8] >= userDeckResource.CardsStats[cardKey][7]):
+		return
+	userDeckResource.CardsStats[cardKey][8] += 1
+	ResourceSaver.save(userDeckResource,"res://Assets/Database/User/UserCardRes.tres")
+	userCardDeck.text = "In Deck: " + str(cardInfo[8])
+	
+func cardRemoved() -> void:
+	if(userDeckResource.CardsStats[cardKey][8] <= 0):
+		return
+	userDeckResource.CardsStats[cardKey][8] -= 1
+	ResourceSaver.save(userDeckResource,"res://Assets/Database/User/UserCardRes.tres")
+	userCardDeck.text = "In Deck: " + str(cardInfo[8])
 
 func set_border_image(type) -> String:
 	if(type == "Normal"):
