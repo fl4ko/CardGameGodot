185a6de9d0e4957856c58fb2234b1acd64fe0179
fl4ko <pyotr.narevsky@gmail.com>
2024-12-08 00:31:48 +0100

    okej wyswietalnie kart

--- Assets/Scripts/game_screen.gd
+++ Assets/Scripts/game_screen.gd
@@ -1,17 +1,12 @@
 extends Node2D
 
-const cardSize = Vector2(150, 225)
+const cardSize = Vector2(125, 187.50)
 
 @onready var userDeckResource : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
 @onready var cardBase = preload("res://Assets/Scenes/card_base.tscn")
 
-@onready var viewportSize = Vector2(get_viewport().size)
-@onready var centerCardOval = viewportSize * Vector2(0.5, 1.2)
-@onready var horizontalRadius = viewportSize.x * 0.45
-@onready var verticalRadius = viewportSize.y * 0.58
+@onready var screen_size = Vector2(get_viewport().size)
 
-var ovalAngle = Vector2()
-var angle = deg_to_rad(90) - 0.6
 var currentCards
 
 func _ready() -> void:
@@ -19,18 +14,23 @@ func _ready() -> void:
 	currentCards = userDeckResource.CardsStats.duplicate(true)
 
 func draw_card():
-	var newCardBase = cardBase.instantiate()
-	var card = select_random_card()
-	print(card)
-	
-	newCardBase.cardName = card[0]
-	ovalAngle = Vector2(horizontalRadius * cos(angle), -verticalRadius * sin(angle))
-	newCardBase.position = centerCardOval + ovalAngle - newCardBase.size/2
-	newCardBase.scale *= cardSize/newCardBase.size
-	$Cards.add_child(newCardBase)
-	
-	angle += 0.2
-	card[8] -= 1
+	var amountOfChildren = $Cards.get_child_count()
+	if amountOfChildren < 7:
+		var newCardBase = cardBase.instantiate()
+		var card = select_random_card()
+		
+		if card.size() > 0:
+			print(card)
+			var spacing = cardSize.x * 1.1  # Adjust spacing between cards
+			var start_x = cardSize.x / 2  # Start from the left side with some margin
+			var position_x = start_x + amountOfChildren * spacing
+			var position_y = screen_size.y - cardSize.y - 50  # Adjust 50 for bottom margin
+			
+			newCardBase.cardName = card[0]
+			newCardBase.position = Vector2(position_x, position_y)
+			newCardBase.scale *= cardSize/newCardBase.size
+			$Cards.add_child(newCardBase)
+			card[8] -= 1
 	return get_total_amount_of_cards()
 
 func select_random_card() -> Array:
