70c2ed064866c8dde1a7a42fd440cb87bb3d2357
fl4ko <pyotr.narevsky@gmail.com>
2024-12-08 02:01:26 +0100

    drawowanie kart jak z ballatro

--- Assets/Scripts/game_screen.gd
+++ Assets/Scripts/game_screen.gd
@@ -2,37 +2,74 @@ extends Node2D
 
 const cardSize = Vector2(125, 187.50)
 
+@export var card_offset_x: float = 50.0
+@export var rot_max: float = 10.0
+@export var anim_offset_y: float = 0.3
+
 @onready var userDeckResource : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
 @onready var cardBase = preload("res://Assets/Scenes/card_base.tscn")
 
 @onready var screen_size = Vector2(get_viewport().size)
 
 var currentCards
+var spacing = cardSize.x * 1.1
+var start_x = cardSize.x / 2
+
+var sine_offset_mult: float = 0.0
+@onready var tween: Tween
 
 func _ready() -> void:
 	print(userDeckResource.CardsStats.keys())
 	currentCards = userDeckResource.CardsStats.duplicate(true)
+	rot_max = deg_to_rad(rot_max)
 
-func draw_card():
-	var amountOfChildren = $Cards.get_child_count()
-	if amountOfChildren < 7:
+#func draw_card(amountToDraw: int):
+	#for i in range(0, amountToDraw):
+		#var amountOfChildren = $Cards.get_child_count()
+		#if amountOfChildren < 12:
+			#var card = select_random_card()
+			#print(card)
+			#if card.size() > 0:
+				#var newCardBase = cardBase.instantiate()
+				#if amountOfChildren < 6:
+					#var position_x = start_x + amountOfChildren * spacing
+					#var position_y = screen_size.y - cardSize.y - 135
+					#newCardBase.position = Vector2(position_x, position_y)
+				#else:
+					#var position_x = start_x + (amountOfChildren-6) * spacing
+					#var position_y = screen_size.y - cardSize.y - 200
+					#newCardBase.position = Vector2(position_x, position_y)
+				#newCardBase.cardName = card[0]
+				#newCardBase.scale *= cardSize/newCardBase.size
+				#$Cards.add_child(newCardBase)
+				#card[8] -= 1
+	#return get_total_amount_of_cards()
+	
+func draw_card(amountToDraw: int, fromPos: Vector2):
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	
+	for i in range(amountToDraw):
 		var newCardBase = cardBase.instantiate()
 		var card = select_random_card()
+		newCardBase.global_position = fromPos
+		newCardBase.scale *= cardSize/newCardBase.size
 		
-		if card.size() > 0:
-			print(card)
-			var spacing = cardSize.x * 1.1  # Adjust spacing between cards
-			var start_x = cardSize.x / 2  # Start from the left side with some margin
-			var position_x = start_x + amountOfChildren * spacing
-			var position_y = screen_size.y - cardSize.y - 50  # Adjust 50 for bottom margin
-			
-			newCardBase.cardName = card[0]
-			newCardBase.position = Vector2(position_x, position_y)
-			newCardBase.scale *= cardSize/newCardBase.size
-			$Cards.add_child(newCardBase)
-			card[8] -= 1
-	return get_total_amount_of_cards()
+		var final_pos: Vector2 = -(newCardBase.size / 2.0) - Vector2(card_offset_x * (amountToDraw - 1 - i), -550)
+		final_pos.x += ((card_offset_x * (amountToDraw-1)) / 2.0) + 550
 
+		var rot_radians: float = lerp_angle(-rot_max, rot_max, float(i)/float(amountToDraw-1))
+		
+		tween.parallel().tween_property(newCardBase, "position", final_pos, 0.3 + (i * 0.075))
+		tween.parallel().tween_property(newCardBase, "rotation", rot_radians, 0.3 + (i * 0.075))
+		
+		newCardBase.cardName = card[0]
+		$Cards.add_child(newCardBase)
+		
+	tween.tween_callback(set_process.bind(true))
+	tween.tween_property(self, "sine_offset_mult", anim_offset_y, 1.5).from(0.0)
+	
 func select_random_card() -> Array:
 	if currentCards.size() <= 0:
 		return []
@@ -49,11 +86,3 @@ func select_random_card() -> Array:
 		
 	randomize()
 	return currentCards[validCards[randi() % validCards.size()]]
-
-func get_total_amount_of_cards():
-	var totalCardsCount = 0
-	for key in currentCards:
-		var card = currentCards[key]
-		totalCardsCount += card[8]
-	print(totalCardsCount)
-	return totalCardsCount
--- Assets/Scripts/hand_draw.gd
+++ Assets/Scripts/hand_draw.gd
@@ -2,9 +2,10 @@ extends TextureButton
 
 @onready var cardSize = $'../../'.cardSize
 @onready var userDeckResource : userCardsResource = preload("res://Assets/Database/User/UserCardRes.tres")
-
 var cardKey
 var cardInfo
+var isFirstDraw = true;
+var totalAmountOfCards = 0
 
 func _ready() -> void:
 	scale *= cardSize/size
@@ -12,7 +13,15 @@ func _ready() -> void:
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-			var totalAmountOfCards = $'../../'.draw_card()
+			if isFirstDraw:
+				$'../../'.draw_card(get_total_amount_of_cards(), position)
+		if(totalAmountOfCards <= 0):
+			disabled = true
 			
-			if(totalAmountOfCards <= 0):
-				disabled = true
+func get_total_amount_of_cards():
+	var totalCardsCount = 0
+	for key in userDeckResource.CardsStats.keys():
+		var card = userDeckResource.CardsStats[key]
+		totalCardsCount += card[8]
+	print(totalCardsCount)
+	return totalCardsCount
